d5f2e1ffa2aa83e33ea69217fb3230a359fcc2f1
Ioannis Maliouris <giannism@e-avenue.eu>
2024-03-24 20:44:54 +0200

    fix: moves arent stored properly in database

--- app/Observers/PieceObserver.php
+++ app/Observers/PieceObserver.php
@@ -7,7 +7,22 @@ use App\Models\Piece;
 
 class PieceObserver
 {
-    public function saved(Piece $piece) {
-        $piece->createMoveIfNecessary();
+    public function created(Piece $piece) {
+        $move = new Move();
+        $move->positionX = $piece->positionX;
+        $move->positionY = $piece->positionY;
+
+        $piece->moves()->save($move);
+    }
+
+    public function updated(Piece $piece) {
+        $changes = $piece->getChanges();
+        if (array_key_exists('positionX', $changes) || array_key_exists('positionY', $changes)) {
+            $move = new Move();
+            $move->positionX = $piece->positionX;
+            $move->positionY = $piece->positionY;
+
+            $piece->moves()->save($move);
+        };
     }
 }
